d78a6b1b4af94ec848c9356f9fd348682b8db106
Vinicius Mota <motavinicius@outlook.com>
2023-08-16 16:54:52 -0300

    versão 1.1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*animacao suave de efeito ancora*/
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -7,3 +9,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+
+  /*executar a animacao apos carregar todo o css (evitar bug)*/
+
+  window.addEventListener('load', () => {
+    let title1 = document.querySelector('.h-header h1')
+    let title2 = document.querySelector('.h-header h2')
+    title1.style.display = 'block';
+    title2.style.display = 'block';
+});
